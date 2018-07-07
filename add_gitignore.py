14d78ce5e7f893549d671a2d80350b4cd3a11e5f
fannyvieira <fanny.vieira@ccc.ufcg.edu.br>
2018-07-07 11:40:03 -0300

    add_gitignore.py: init cli

--- add_gitignore.py
+++ add_gitignore.py
@@ -2,13 +2,15 @@
 
 import requests
 import string
+import argparse
 
-BASE_URL = 'https://api.github.com'
+SEARCH_URL = 'https://api.github.com/gitignore/templates'
+HEADER = {'Accept': 'application/vnd.github.v3.raw+json'}
+FILE_NAME = '.gitignore'
 
 
-def write_file():
-    with open('.gitignore', 'w') as f:
-        data = get_template_by_language('c')['source']
+def write_file(data):
+    with open(FILE_NAME, 'a') as f:
         f.write(data)
 
 
@@ -16,9 +18,7 @@ def get_all_templates():
     """Get all gitignore templates available
         :return response: Json with templates
     """
-    url = '{baseurl}/gitignore/templates'.format(baseurl=BASE_URL)
-    headers = {'Accept': 'application/vnd.github.v3.raw+json'}
-    r = requests.get(url, headers)
+    r = requests.get(SEARCH_URL, HEADER)
     return r.json()
 
 
@@ -29,13 +29,29 @@ def get_template_by_language(language):
         :return response: Json with specified template
     """
     language = string.capwords(language)
-    url = '{baseurl}/gitignore/templates/{lang}'.format(baseurl=BASE_URL, lang=language)
-    headers = {'Accept': 'application/vnd.github.v3.raw+json'}
-    r = requests.get(url, headers)
+    url = '{base_url}/{lang}'.format(base_url=SEARCH_URL, lang=language)
+    r = requests.get(url, HEADER)
     return r.json()
 
 
+def get_parser():
+    parser = argparse.ArgumentParser(description='Generate Gitignore rules basing on GitHub"s templates collection')
+    parser.add_argument("-l", "--language", help="Get the gitignore template specified", nargs='+')
+    return parser
+
+
+def command_line_runner():
+    parser = get_parser()
+    args = vars(parser.parse_args())
+    if args['language']:
+        for language in args['language']:
+            data = get_template_by_language(language)
+            write_file(data['source'])
+    else:
+        parser.print_help()
+
+
 if __name__ == '__main__':
-    write_file()
+    command_line_runner()
 
 
