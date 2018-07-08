4a3b5c9147053423db161680bff2a45b4829205a
fannyvieira <fanny.vieira@ccc.ufcg.edu.br>
2018-07-08 11:30:12 -0300

    run.py: change run to module because pypy is not recognized script

--- run.py
+++ run.py
@@ -1,27 +1,4 @@
-import argparse
-import textwrap
-from add_gitignore.add_gitignore import *
-
-
-def get_parser(choices):
-    parser = argparse.ArgumentParser(description='Generate Gitignore rules basing on GitHub"s templates collection')
-    parser.add_argument("-v", "--view", help='view languages available', action='store_true')
-    parser.add_argument("-l", "--language", nargs='+', help='Get the language gitignore '
-                                                            'template specified:\n%s'
-                                                            % (textwrap.dedent('\n'.join(choices))))
-    return parser
-
-
-def command_line_runner():
-    choices = get_all_templates()
-    parser = get_parser(choices)
-    args = vars(parser.parse_args())
-    if args['language']:
-        create_gitignore(args['language'])
-    elif args['view']:
-        print choices
-    else:
-        parser.print_help()
+from add_gitignore import command_line_runner
 
 
 if __name__ == '__main__':
