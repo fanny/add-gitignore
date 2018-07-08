92fca7721a127bc84297f32c878c9bb76b3ecd2d
fannyvieira <fanny.vieira@ccc.ufcg.edu.br>
2018-07-08 11:31:37 -0300

    setup.py: requirement of pypy

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+import add_gitignore
+
+setup(
+    name="add_gitignore",
+    version="0.9.0",
+    author="fanny vieira",
+    author_email="fannyvieira082@gmail.com",
+    packages=find_packages(),
+    license='MIT License',
+    url="https://github.com/FannyVieira/add-gitignore",
+    entry_points={
+        'console_scripts': [
+            'add-gitignore=add_gitignore:command_line_runner',
+        ]
+    },
+    classifiers=(
+        "Programming Language :: Python :: 2.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+    description="Generate Gitignore rules basing on GitHub's templates collection."
+)
