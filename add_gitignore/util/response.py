ce405dcb4c4efaa162b43323aaea241ecd9e8e3f
Fanny Vieira <fanny>
2018-07-09 09:25:31 -0300

    response.py: Add documentation

--- add_gitignore/util/response.py
+++ add_gitignore/util/response.py
@@ -2,9 +2,14 @@
 
 
 def response_sucess():
+    """Creates a sucess response that indicates the gitignore is generated
+    """
     print "the gitignore template is generated"
 
 
 def response_error(lang):
+    """Creates a error response that indicates the language was not writted
+    :param lang: the languages was not be writted in gitignore file
+    """
     print "The %s language specified is not writted because is not available " \
           "check more in: add_gitignore.py --help" % lang
