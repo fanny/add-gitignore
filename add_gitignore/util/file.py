268d16bb2b859bd2c652b50e6310398b41e16da1
Fanny Vieira <fanny>
2018-07-09 09:25:17 -0300

    file.py: Add documentation

--- add_gitignore/util/file.py
+++ add_gitignore/util/file.py
@@ -4,12 +4,22 @@ FILE_NAME = '.gitignore'
 
 
 def read_file():
+    """Read gitignore file and return its data.
+    :return: gitignore data
+    """
     with open(FILE_NAME) as f:
         data = f.read()
     return data
 
 
 def write_file(data):
+    """Write in gitignore file, using append mode,
+    namely, whenever new information
+    is passed is inserted at the end,
+    if the information already exists
+    in the file will not be inserted.
+    :param data: the data to be insert in gitignore file
+    """
     with open(FILE_NAME, 'a') as f:
         if not (data in read_file()):
             f.write(data)
